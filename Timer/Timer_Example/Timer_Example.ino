23ea30cfc28affff547997a8bc63f7efdafb11fc
TelevisionNinja <televisionninja@gmail.com>
2020-02-28 01:47:57 -0600

    comments

--- Timer/Timer_Example/Timer_Example.ino
+++ Timer/Timer_Example/Timer_Example.ino
@@ -3,13 +3,17 @@ Timer timer;
 
 void setup() {
   Serial.begin(9600);
+
+  // test for getting the time after it is stopped
   timer.start();
-  delay(2000);
+  delay(1000);
   timer.stop();
-  Serial.print(timer.getTime(false));
+  Serial.print(timer.getTime(false));//check bool is set to false to get the stopped time
+
+  // test for checing the time while it is still running
   timer.start();
-  delay(3000);
-  Serial.print(timer.getTime(true));
+  delay(1000);
+  Serial.print(timer.getTime(true));//check bool is set to true to check the time
   timer.stop();
 }
 
