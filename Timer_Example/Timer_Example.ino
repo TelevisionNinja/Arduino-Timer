fb8b8d8d296c5e338fd93b25bd5345ff6fabfa6a
TelevisionNinja <televisionninja@gmail.com>
2020-03-06 14:46:11 -0600

    added loop check example

--- Timer_Example/Timer_Example.ino
+++ Timer_Example/Timer_Example.ino
@@ -8,15 +8,21 @@ void setup() {
   timer.start();
   delay(1000);
   timer.stop();
-  Serial.print(timer.getTime(false));//check bool is set to false to get the stopped time
+  Serial.println(timer.getTime(false));//check bool is set to false to get the stopped time
 
   // test for checing the time while it is still running
   timer.start();
   delay(1000);
-  Serial.print(timer.getTime(true));//check bool is set to true to check the time
+  Serial.println(timer.getTime(true));//check bool is set to true to check the time
   timer.stop();
+
+  timer.start();//start the timer for the loop example
 }
 
 void loop() {
-  
+  //loop example
+  if (timer.getTime(true) >= 1000) {//every set time, perform an action
+    Serial.println("check inside a loop");
+    timer.start();//no need to stop and reset the timer, just call start() to set a new initial starting point
+  }
 }
